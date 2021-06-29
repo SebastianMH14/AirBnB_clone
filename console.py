b7a212720571eebc882ff3f6ce64e09b454cdd6b
SebastianMH14 <sebasmh2002@hotmail.com>
2021-06-29 17:17:01 -0500

    fix pep8 console

--- console.py
+++ console.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""print prompt"""
-"""environ cmd"""
-"""print Quit command to exit the program whit help quit"""
-"""emtyline whit enter"""
+"""creating a console command interpreter"""
 
 import cmd
 
@@ -10,20 +7,17 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """command interpreter"""
     prompt = "(hbnb) "
-    """Quit command to exit the program"""
-    def do_quit(self, args):
-        'Quit command to exit the program'
-        return True
 
+    def do_quit(self, args):
+        "Quit command to exit the program"
+        return True
 
     def do_EOF(Self, args):
-        'End Of File'
+        "End Of File"
         return True
-
 
     def emptyline(self):
         pass
-
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
