a66d64248da734439d2c915d619b6fcde78ca367
SebastianMH14 <sebasmh2002@hotmail.com>
2021-06-29 16:11:33 -0500

    fix kwards

--- models/base_model.py
+++ models/base_model.py
@@ -10,15 +10,16 @@ class BaseModel:
         """Initialization of the base model"""
 
         if kwargs:
-            self.__dict__ = kwargs
-            if "created_at" in kwargs:
-                self.created_at = self.__dict__["created_at"] = datetime.now()
-            if "updated_at" in kwargs:
-                self.updated_at = self.__dict__["updated_at"] = datetime.now()
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "create_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """String representation of the class itself."""
