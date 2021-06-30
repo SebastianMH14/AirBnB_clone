7ddf2704e5e1194d5c6b9640bf492099208bf91b
SebastianMH14 <sebasmh2002@hotmail.com>
2021-06-30 18:16:27 -0500

    remove hasatr

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,6 +12,5 @@ class TestBaseModel(unittest.TestCase):
         bm1 = BaseModel()
         bm2 = BaseModel()
         self.assertIsInstance(bm1, BaseModel)
-        self.assertTrue(hasattr(bm1, "id"))
         self.assertNotEqual(bm1.id, bm2.id)
         self.assertIsInstance(bm1.id, str)
