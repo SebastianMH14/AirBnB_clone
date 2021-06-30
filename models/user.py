d146b47db3818d6564e8c49561591046c25f0f9c
SebastianMH14 <sebasmh2002@hotmail.com>
2021-06-30 18:43:20 -0500

    fix user herencia

--- models/user.py
+++ models/user.py
@@ -2,8 +2,10 @@
 """Creating a user class"""
 """empty strings"""
 
+from models import base_model
+from models.base_model import BaseModel
 
-class User:
+class User(BaseModel):
     """class user"""
     email = ""
     password = ""
