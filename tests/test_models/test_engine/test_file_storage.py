dfd448b106276bfa8df8053c4118b028f68de761
SebastianMH14 <sebasmh2002@hotmail.com>
2021-06-30 20:15:06 -0500

    add test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,6 +2,7 @@
 """file storage test"""
 import unittest
 from models.engine.file_storage import FileStorage
+import pep8
 
 
 class FileStorage_test(unittest.TestCase):
@@ -14,3 +15,9 @@ class FileStorage_test(unittest.TestCase):
         self.assertTrue(len(FileStorage.all.__doc__) > 0)
         self.assertTrue(len(FileStorage.a.__doc__) > 0)
         self.assertTrue(len(FileStorage.all.__doc__) > 0)
+
+    def test_pep8_user(self):
+        """test pep8s"""
+        style = pep8.StyleGuide(quiet=True)
+        v = style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(v.total_errors, 0, "fix pep8")
